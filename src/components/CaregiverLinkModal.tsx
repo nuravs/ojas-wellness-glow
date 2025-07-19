6ea62ce211862bdc055716f8d207f157c76be301
nuravs <94032180+nuravs@users.noreply.github.com>
2025-07-19 12:57:53 +0530

    Create CaregiverLinkModal.tsx
    Created CaregiverLinkModal.tsx component to fix missing import in HomePage

---
+++ src/components/CaregiverLinkModal.tsx
@@ -0,0 +1,28 @@
+// src/components/CaregiverLinkModal.tsx
+
+import React from 'react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from './ui/dialog';
+import { Button } from './ui/button';
+
+interface CaregiverLinkModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export const CaregiverLinkModal: React.FC<CaregiverLinkModalProps> = ({ open, onClose }) => {
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Link a Caregiver</DialogTitle>
+          <DialogDescription>
+            This feature will allow patients to securely link their caregiver for support.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="mt-4 flex justify-end">
+          <Button onClick={onClose}>Close</Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
